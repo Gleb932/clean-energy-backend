e8687498326ae5c03d249aed50bff5bcba46ec48
Gleb932 <gleb.g932@gmail.com>
2025-12-11 15:01:24 +0100

    Init

---
+++ src/main/java/com/codibly/clean_energy/CleanEnergyApplication.java
@@ -0,0 +1,13 @@
+package com.codibly.clean_energy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CleanEnergyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CleanEnergyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codibly/clean_energy/CleanEnergyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codibly.clean_energy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CleanEnergyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
